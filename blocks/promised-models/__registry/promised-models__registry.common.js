f23f13a4492fd177891eb23da5826d43a402b2ca
Vova Bilonenko <bilonenko.v@gmail.com>
2015-05-27 11:43:22 +0300

    decl models

--- blocks/promised-models/__registry/promised-models__registry.common.js
+++ blocks/promised-models/__registry/promised-models__registry.common.js
@@ -3,5 +3,71 @@
  */
 
 (function (Model) {
-    console.log('Model', Model);
+
+    /**
+     * model with declarative format for attributes
+     * @class {Model}
+     */
+    var DeclarativeModel = Model.inherit({
+
+        /**
+         * @override
+         */
+        __constructor: function () {
+            var model = this;
+            this.attributes = Object.keys(this.attributes || {}).reduce(function (attributes, attributeName) {
+                var attributeDecl = model.attributes[attributeName],
+                    type = attributeDecl.type,
+                    attributeClass;
+                if (attributeDecl instanceof Model.Attribute) {
+                    attributeClass = attributeDecl;
+                } else if (Model.attributeTypes[type]) {
+                    attributeClass = Model.attributeTypes[type].inherit(attributeDecl);
+                } else if (BEM.blocks[type]) {
+                    attributeClass = BEM.blocks[type].inherit(attributeDecl);
+                } else {
+                    throw new Error('Unknown attribute type:' + type);
+                }
+                attributes[attributeName] = attributeClass;
+                return attributes;
+            }, {});
+            this.__base.apply(this, arguments);
+        }
+
+    });
+
+    /**
+     * decl model
+     * @param  {string} blockName
+     * @param  {string} [baseBlock]
+     * @param  {Object} props
+     * @param  {Object} [staticProps]
+     */
+    Model.decl = function (blockName, baseBlock, props, staticProps) {
+        var baseClass = DeclarativeModel;
+        if (typeof baseBlock !== 'string') {
+            staticProps = props;
+            props = baseBlock;
+            baseBlock = null;
+        }
+        if (BEM.blocks[blockName]) {
+            baseClass = BEM.blocks[blockName];
+        } else if (baseBlock) {
+            baseClass = BEM.blocks[baseBlock];
+        }
+
+        BEM.blocks[blockName] = baseClass.inherit(props, staticProps).inherit({
+            attributes: jQuery.extend({}, baseClass.prototype.attributes || {}, props.attributes)
+        });
+
+        /**
+         * create model instance
+         * @param  {string|number} [id]
+         * @param  {object} [data]
+         * @return {BEM.Model}
+         */
+        BEM.blocks[blockName].create = function (id, data) {
+            return new BEM.blocks[blockName](id, data);
+        };
+    };
 }(BEM.Model));
--- test.blocks/test/test.common.js
+++ test.blocks/test/test.common.js
@@ -6,6 +6,7 @@
 /* global mochaPhantomJS */
 /* global chai */
 /* global describe */
+/* global Vow */
 
 BN.addDecl('test', 'page', {
     route: /^\/$/
@@ -18,8 +19,20 @@ BN.addDecl('test', 'page', {
             block: 'test',
             js: true
         });
+    },
+
+    /**
+     * @override
+     */
+    update: function () {
+        return Vow.reject();
     }
+
 }).instanceProp({
+
+    /**
+     * @override
+     */
     init: function () {
         var expect = chai.expect;
         mocha.ui('bdd');
@@ -33,21 +46,52 @@ BN.addDecl('test', 'page', {
         }
     },
 
+    /**
+     * decl tests
+     * @param  {Chai.expect} expect
+     */
     test: function (expect) {
-        describe('model decl', function () {
-            before(function () {
-                BEM.Model.delc('model', {
+        describe('BEM.Model', function () {
+            beforeEach(function () {
+                delete BEM.blocks['model'];
+                BEM.Model.decl('model', {
                     attributes: {
                         a: {
-                            type: 'string',
+                            type: 'String',
                             default: 'a'
                         }
                     }
                 });
             });
+            describe('decl', function () {
+                it('should add model class to BEM.blocks', function () {
+                    expect(BEM.blocks['model']).to.be.not.a('undefined');
+                });
 
-            it('should add model class to BEM.blocks', function () {
-                expect(BEM.blocks['model']).to.be.not.a('undefined');
+                it('should extend model fields and methods in case of multiple decls', function () {
+                    BEM.Model.decl('model', {
+                        attributes: {
+                            b: {
+                                type: 'String',
+                                default: 'b'
+                            }
+                        }
+                    });
+                    var model = BEM.blocks['model'].create();
+                    expect(model.get('a')).to.be.equal('a');
+                    expect(model.get('b')).to.be.equal('b');
+                });
+
+                describe('model', function () {
+                    it('should be instanceof BEM.Model', function () {
+                        expect(BEM.blocks['model'].prototype instanceof BEM.Model).to.be.equal(true);
+                    });
+                });
+            });
+            describe('create', function () {
+                it('should create model instance', function () {
+                    expect(BN('model').create() instanceof BEM.Model).to.be.equal(true);
+                });
             });
         });
     }
