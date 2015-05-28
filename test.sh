20752ff72114a0b6e2c97b71d442e8afe6616e82
Vova Bilonenko <bilonenko.v@gmail.com>
2015-05-28 18:52:28 +0300

    fix test

--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-PHANTOM='./node_modules/mocha-phantomjs/bin/mocha-phantomjs http://127.0.0.1:3000/'
+PHANTOM='./node_modules/mocha-phantomjs/bin/mocha-phantomjs http://127.0.0.1:3000/ '
 BEMNODE='node test.app/app/app.server.js --socket 3000'
 
 function checkfail {
     EXIT_CODE=$1
     if [ $EXIT_CODE -gt 0 ]; then
        echo exit code $EXIT_CODE
+       killall node 2>/dev/null
        exit $EXIT_CODE;
     fi
 }
@@ -14,5 +15,4 @@ function checkfail {
 killall node 2>/dev/null
 node lib/browserify.js
 $BEMNODE & $PHANTOM
-killall node 2>/dev/null
 checkfail $?
