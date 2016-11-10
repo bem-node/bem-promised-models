9021c53c859cdbb5d27a546ea068fac6e8eb1540
Дмитрий Канатников <steve111@yandex.ru>
2016-11-11 02:58:46 +0300

    mustDeps from own block's elements breaks deps tech from enb

--- blocks/promised-models/promised-models.deps.js
+++
@@ -1,7 +0,0 @@
-{
-    mustDeps: [
-        {block: 'promised-models', elem: 'model'},
-        {block: 'promised-models', elem: 'registry'},
-        {block: 'promised-models', elem: 'registry-storage'}
-    ]
-}
